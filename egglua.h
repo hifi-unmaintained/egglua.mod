846eaf6792442c205c94701ce903c808aac1244d
Toni Spets <toni.spets@iki.fi>
2010-01-30 10:38:59 +0200

    Add more locations for Lua headers

--- egglua.h
+++ egglua.h
@@ -6,9 +6,9 @@
 
 #include <stdlib.h>
 
-#include <lua5.1/lua.h>
-#include <lua5.1/lualib.h>
-#include <lua5.1/lauxlib.h>
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 
 static lua_State *L = NULL;
 
